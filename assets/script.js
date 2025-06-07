b3afc7e7c1b7982e847fbe149b96f92eb26f5a58
Thesancs <santos.glauber2002@gmail.com>
2025-06-06 22:34:18 -0300

    Organize video layout and place CTA below

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,19 @@ async function loadComponent(id, path) {
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadComponent('header', './components/header.html');
-  await loadComponent('main', './components/video.html');
+
+  const mainEl = document.getElementById('main');
+  const videoRes = await fetch('./components/video.html');
+  mainEl.innerHTML = await videoRes.text();
   const playerScript = document.createElement('script');
-  playerScript.src = 'https://scripts.converteai.net/c9da0398-934e-4680-99b9-d4888d7094af/players/6842141d891666ff59083fee/player.js';
+  playerScript.src =
+    'https://scripts.converteai.net/c9da0398-934e-4680-99b9-d4888d7094af/players/6842141d891666ff59083fee/player.js';
   playerScript.async = true;
   document.head.appendChild(playerScript);
-  await loadComponent('cta', './components/cta.html');
+
+  const ctaRes = await fetch('./components/cta.html');
+  mainEl.insertAdjacentHTML('beforeend', await ctaRes.text());
+
   await loadComponent('footer', './components/footer.html');
 
   setTimeout(() => {
